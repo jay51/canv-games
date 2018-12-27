4daa1a13b2d87ae2706ad235c3c60ae088e361ad
jay aljoe <nenojaafar3@gmail.com>
2018-12-26 20:00:20 -0600

    Update index.js
    Added the first steps to start with the drawing function

--- draw_board/index.js
+++ draw_board/index.js
@@ -1,3 +1,29 @@
 // make Canvas and context
+// recorde x and y positions 
+let prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
+
+let canv = document.createElement("canvas");
+let ctx = canv.getContext("2d");
+document.body.appendChild(canv);
+
 // draw a white cerical onMouseDown
+canv.addEventListener("mosuedown", function(e){
+  console.log(e);
+  //listen for mosuemove and draw   
+});
+
+function draw() {
+  ctx.beginPath();
+  ctx.moveTo(x,y);
+  ctx.lineTo(x,y);
+  ctx.strokStyle = "black";
+  ctx.lineWidth = 2;
+  ctx.strok();
+  ctx.closePath();
+}
+  
 // draw a canvas color cerical/transparent/delete onSomeMouseEvent
