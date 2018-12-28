8f984daad7c24b012ec8f1c66bae8f813c455162
jay51 <nenojaafar3@gmail.com>
2018-12-28 14:16:18 -0600

    stored the prevY/X and connected them to the current X/Y and improved on some features

--- draw_board/index.js
+++ draw_board/index.js
@@ -8,18 +8,29 @@ let ctx = canv.getContext("2d");
 document.body.appendChild(canv);
 
 // recorde x and y positions
-let prevX = 0,
-	currX = 0,
-	prevY = 0,
-	currY = 0,
-	dot_flag = false;
+let prevX, prevY;
+let lineWidth = 25;
 
 function draw(e) {
 	ctx.beginPath();
 	ctx.fillStyle = "white";
-	ctx.arc(e.x, e.y, 10, 0, Math.PI * 2, false);
+	ctx.arc(e.x, e.y, lineWidth / 2, 0, Math.PI * 2);
 	ctx.fill();
 	ctx.closePath();
+	ctx.beginPath();
+
+	if (prevX && prevY) {
+		ctx.moveTo(prevX, prevY);
+		ctx.strokeStyle = "white";
+		ctx.lineTo(e.x, e.y);
+		ctx.lineWidth = lineWidth;
+		ctx.stroke();
+		ctx.closePath();
+	}
+
+	// store the next point
+	prevY = e.y;
+	prevX = e.x;
 }
 
 // draw a white cerical onMouseDown
@@ -29,19 +40,11 @@ canv.addEventListener("mousedown", function(e) {
 });
 
 canv.addEventListener("mouseup", function() {
+	prevX = null;
+	prevY = null;
 	// remove the mousemove event
 	canv.removeEventListener("mousemove", draw);
 });
-
-// function draw() {
-// 	ctx.beginPath();
-// 	ctx.moveTo(x, y);
-// 	ctx.lineTo(x, y);
-// 	ctx.strokStyle = "black";
-// 	ctx.lineWidth = 2;
-// 	ctx.strok();
-// 	ctx.closePath();
-// }
 
 // draw a canvas color
 // cerical/transparent/delete onSomeMouseEvent
